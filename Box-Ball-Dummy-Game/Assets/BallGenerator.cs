030cb31b66883ae0a77767b5d758554968d8d62c
Akheheh <lx.heidarian@gmail.com>
2020-09-01 15:43:10 -0700

    Collider fixes

--- Box-Ball-Dummy-Game/Assets/BallGenerator.cs
+++ Box-Ball-Dummy-Game/Assets/BallGenerator.cs
@@ -9,6 +9,8 @@ public class BallGenerator : MonoBehaviour
     [SerializeField] GameObject boxPrefab;
     [SerializeField] GameObject dummyPrefab;
 
+    [SerializeField] GameObject Bouncer;
+    
     public void spewObject(Vector2 coord, GameObject obj) {
         GameObject objCopy = Instantiate(obj);
         objCopy.transform.position = coord;
@@ -16,22 +18,39 @@ public class BallGenerator : MonoBehaviour
         rigidBody.velocity = new Vector2(0.5f,0);
     }
 
+    void Destroyer() {
+        Destroy(this.gameObject);
+    }
+
+    public void getPointer() {
+        BoxCollider2D rigby = Bouncer.GetComponent<BoxCollider2D>();
+        rigby.transform.position = Input.mousePosition;
+    }
+    public void Start() {
+        
+    }
+
     public void Update() {
+        getPointer();
+        
         if(Input.GetKeyDown(KeyCode.Alpha1)) {
             spewObject(Camera.main.ScreenToWorldPoint(Input.mousePosition), ballPrefab);
+            
         }
         
         if(Input.GetKeyDown(KeyCode.Alpha2)) {
             spewObject(Camera.main.ScreenToWorldPoint(Input.mousePosition), boxPrefab);
         }
 
-        if(Input.GetKeyDown(KeyCode.Space)) {
+        if(Input.GetKeyDown(KeyCode.Alpha3)) {
             spewObject(Camera.main.ScreenToWorldPoint(Input.mousePosition), dummyPrefab);
         }
+
 
 
         if(Input.GetKeyDown(KeyCode.Escape)) {
             Application.Quit();
         }
+
     }
 }
